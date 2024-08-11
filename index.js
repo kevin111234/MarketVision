eee4a7f1e09400af3c0aba200cdebcacccb9777c
woghd111234 <jaehong243587@gmail.com>
2024-08-12 01:42:53 +0900

    express 기반 웹서버 재설정

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
-const http = require('http');
-const fs = require('fs').promises;
+const express = require('express');
 
-const server = http.createServer(async (req, res) =>{
-  try {
-    const data = await fs.readFile('./index.html');
-    res.writeHead(200, {'Content-Type': 'text/HTML; charset=utf-8'});
-    res.end(data);
-  } catch (err) {
-    console.error(err);
-    res.writeHead(500, {'Content-Type': 'text/plain; charset=utf-8'});
-    res.end(err.message);
-  }
-});
-server.listen(8000)
+const app = express();
+app.set('port', process.env.PORT || 8000);
 
-server.on('listening', () => {
-  console.log('8000번 포트에서 서버 대기중입니다!');
+app.get('/', (req, res) =>{
+  res.send('Hello, Express');
 });
-server.on('error', (error) => {
-  console.log(error);
-});
+
+app.listen(app.get('port'), ()=> {
+  console.log(app.get('port'), '번 포트에서 대기 중')
+})
