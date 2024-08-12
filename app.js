050d6ebfdd51d4ebb05bba2dad7631990e5dcca9
woghd111234 <jaehong243587@gmail.com>
2024-08-12 12:23:37 +0900

    에러 발생 미들웨어 설정

--- app.js
+++ app.js
@@ -4,17 +4,15 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const dotenv = require('dotenv');
 const path = require('path');
-const nunjucks = require('nunjucks')
+const nunjucks = require('nunjucks');
 
 // .env 설정 불러오기
 dotenv.config();
 const app = express();
-// 라우팅 호출
 const indexRouter = require('./routes');
 
 // port, view 설정
 app.set('port', process.env.PORT || 8000);
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 nunjucks.configure('views', {
   express: app,
@@ -28,8 +26,16 @@ app.use('/', indexRouter);
 
 // error 발생 시
 app.use((req, res, next) => {
-  res.status(404).send('Not Found')
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
 })
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 app.listen(app.get('port'), ()=> {
   console.log(app.get('port'), '번 포트에서 대기 중')
