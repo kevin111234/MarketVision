b2a221eb42232c59142a62d81997ee9a9515a951
woghd111234 <jaehong243587@gmail.com>
2024-08-12 11:16:07 +0900

    server 생성 시 .env 파일 불러오도록 설정

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
+const path = require('path');
 
+dotenv.config();
 const app = express();
 app.set('port', process.env.PORT || 8000);
+
 
 app.get('/', (req, res) =>{
   res.send('Hello, Express');
