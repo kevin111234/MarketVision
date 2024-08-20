8979fef415ec0cf94fd231d794586bb67dc38ad0
woghd111234 <jaehong243587@gmail.com>
2024-08-20 14:57:43 +0900

    대시보드 뷰 수정 및 시장지표 그래프 4개 표시

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 
 router.get('/', async (req, res) => {
   let news = [];
-  let graphDataJson = 'null';
+  let graphDataArray = [];
 
   try {
     // FastAPI에서 뉴스 데이터를 가져옴
@@ -14,17 +14,19 @@ router.get('/', async (req, res) => {
     console.error('Error fetching news:', error.message);
   }
 
-  try {
-    // FastAPI에서 주식 지표 그래프 데이터를 가져옴
-    const graphResponse = await axios.get('http://localhost:8000/stock-index/1/last-three-months');
-    const graphData = graphResponse.data;
-    graphDataJson = JSON.stringify(graphData);
-  } catch (error) {
-    console.error('Error fetching graph data:', error.message);
+  // 1부터 4까지의 index ID에 대한 그래프 데이터 가져오기
+  for (let i = 1; i <= 4; i++) {
+    try {
+      const graphResponse = await axios.get(`http://localhost:8000/stock-index/${i}/last-three-months`);
+      graphDataArray.push(graphResponse.data);
+    } catch (error) {
+      console.error(`Error fetching graph data for index ${i}:`, error.message);
+      graphDataArray.push(null);
+    }
   }
 
   // dashboard 페이지 렌더링, 뉴스와 그래프 데이터를 전달
-  res.render('dashboard', { news, graphDataJson });
+  res.render('dashboard', { news, graphDataArray: JSON.stringify(graphDataArray) });
 });
 
 module.exports = router;
