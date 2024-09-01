d0f17ed68ec2e2e2f2c5a8fd0d0b7f9df72b4506
woghd111234 <jaehong243587@gmail.com>
2024-09-01 17:29:10 +0900

    사용하지 않는 데이터 테이블 주석처리

--- models/Commodity.js
+++ models/Commodity.js
@@ -1,18 +1,19 @@
-const { DataTypes } = require('sequelize');
-
-module.exports = (sequelize) => {
-  return sequelize.define('Commodity', {
-    symbol: {
-      type: DataTypes.STRING(10),
-      allowNull: false,
-      unique: true,
-    },
-    name: {
-      type: DataTypes.STRING(100),
-      allowNull: false,
-    },
-  }, {
-    tableName: 'Commodity',
-    timestamps: false,
-  });
-};
+// const { DataTypes } = require('sequelize');
+// 
+// module.exports = (sequelize) => {
+//   return sequelize.define('Commodity', {
+//     symbol: {
+//       type: DataTypes.STRING(30),
+//       allowNull: false,
+//       unique: true,
+//     },
+//     name: {
+//       type: DataTypes.STRING(100),
+//       allowNull: false,
+//     },
+//   }, {
+//     tableName: 'Commodity',
+//     timestamps: false,
+//   });
+// };
+// 
